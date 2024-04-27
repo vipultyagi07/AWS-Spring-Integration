5555d4dd907e858d80c1e0ad279b34ac2ed97d95
Vipul Tyagi <vipultyagi@Vipuls-MacBook-Air.local>
2024-04-27 17:00:35 +0530

    CHANGE :: package name

---
+++ aws-s3/src/main/java/vip/repository/S3FileMetadataRepository.java
@@ -0,0 +1,11 @@
+package vip.repository;
+
+import vip.modal.S3FileMetadata;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface S3FileMetadataRepository extends JpaRepository<S3FileMetadata,Long> {
+
+    S3FileMetadata findByOriginalFileName(String originalFilename);
+}
---
+++ aws-s3/src/main/java/vip/service/FileService.java
@@ -0,0 +1,17 @@
+package vip.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FileService {
+
+    Map<String,Object> saveFile(MultipartFile file, String userId);
+
+    byte[] downloadFile(String fileName);
+
+    List<String> listAllFiles();
+
+    String deleteFile(String fileName);
+}
